175fc650bafbd54bea4a447854a85c35b8c47b38
Florent (Mac) <grenaille.florent85@gmail.com>
2024-09-13 16:03:41 +0200

    Add comments

--- src/main.cpp
+++ src/main.cpp
@@ -8,21 +8,21 @@ String dataReceived;
 
 
 void setup() {
-  pinMode(tempSensorPin, INPUT);
-  Serial.begin(9600);
+  pinMode(tempSensorPin, INPUT); // Set the arduino board pin A1 to INPUT mode
+  Serial.begin(9600); // Init the Serial communication and set the BAUDRATES transmission to 9600
 }
 
 
 void loop() {
-  temp = readTemperature(tempSensorPin);
+  temp = readTemperature(tempSensorPin); // readTemperature is define in include/sensors.cpp
   Serial.println(temp);
 
 
   if (Serial.available() > 0) {
-    dataReceived = readData();
+    dataReceived = readData(); // readData is define in include/api_communication.cpp
     Serial.print("Donnée reçu du script python : ");
     Serial.println(dataReceived);
   }
 
-  delay(1000); // 1 sec
+  delay(1000); // Wait 1 sec
 }
